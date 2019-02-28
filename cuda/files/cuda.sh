1d5dc387e27f32b2993996bd2e935d1c478b8278
Longwu Ou <oulongwu@mit.edu>
2019-02-27 22:07:45 -0500

    Update cuda to 10.1.105

--- cuda/files/cuda.sh
+++
@@ -1,2 +0,0 @@
-export PATH=$PATH:/usr/share/cuda/bin
-export LD_LIBRARY_PATH=/usr/share/cuda/lib:/usr/share/cuda/lib64:/usr/share/cuda/nvvm/lib:/usr/share/cuda/nvvm/lib64:/usr/share/cuda/extras/CUPTI/lib64
